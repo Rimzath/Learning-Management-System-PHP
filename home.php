f2e3a3c5299877bc810bc95197877de9e6d41d0c
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2024-09-10 14:52:21 +0530

    home page

---
+++ home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Online Library Management System | </title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="home page/css/home.css" rel="stylesheet" />
+
+    <!-- CUSTOM STYLE  -->
+    <link href="home page/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+</head>
+<body>
+    <!------MENU SECTION START-->
+<?php include('header.php');?>
+<!-- MENU SECTION END-->
+<div class="content-wrapper">
+<div class="container">
+<div class="row pad-botm">
+<div class="col-md-12">
+</div>
+</div>
+             
+
+ 
+    </div>
+    </div>
+     <!-- CONTENT-WRAPPER SECTION END-->
+ <?php include('footer.php');?>
+
+
+</body>
+</html>
